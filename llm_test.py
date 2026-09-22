1d71696684a27caf74509362ea8fe973e8bb05bf
Ekanshps <ekanshprataps@gmail.com>
2026-09-22 12:58:23 +0530

    Update LLM test and ignore local secrets

--- llm_test.py
+++ llm_test.py
@@ -1,3 +1,18 @@
 import os
-from .env import load_dotenv
-from openai import OpenAi
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+HF_TOKEN=os.getenv("HF_TOKEN")
+
+
+client=OpenAI(base_url="https://router.huggingface.co/v1",api_key=HF_TOKEN)
+
+responce=client.chat.completions.create(model="openai/gpt-oss-120b",
+                               messages=[{
+                                   "role":"user",
+                                   "content":"What Is good source of Protien in Vegeterian."
+                               }])
+
+answer=responce.choices[0].messsage.content
+print(answer)
